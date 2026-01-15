a0d8c6d778e19083ec6300733799598ae91682ca
MrLarkyy_ <studiotadikl@gmail.com>
2026-01-15 02:26:22 +0100

    Add `inject` method to `MessageCache` and update `set` to handle mutable maps

--- Common/src/main/kotlin/gg/aquatic/klocale/message/MessageCache.kt
+++ Common/src/main/kotlin/gg/aquatic/klocale/message/MessageCache.kt
@@ -8,10 +8,11 @@ interface MessageCache<A : Message<A>> {
     fun getLanguages(): Set<String>
 
     suspend fun set(map: Map<String, Map<String, A>>)
+    suspend fun inject(map: Map<String, Map<String, A>>)
 
     class MapCache<A: Message<A>> : MessageCache<A> {
 
-        private var map: MutableMap<String, Map<String,A>> = HashMap()
+        private var map: MutableMap<String, MutableMap<String,A>> = HashMap()
 
         override fun get(language: String, key: String): A? {
             return map[language]?.get(key)
@@ -30,8 +31,17 @@ interface MessageCache<A : Message<A>> {
         }
 
         override suspend fun set(map: Map<String, Map<String, A>>) {
-            synchronized(map) {
-                this@MapCache.map = map.toMutableMap()
+            synchronized(this.map) {
+                this@MapCache.map = map.mapValues { it.value.toMutableMap() }.toMutableMap()
+            }
+        }
+
+        override suspend fun inject(map: Map<String, Map<String, A>>) {
+            synchronized(this.map) {
+                for ((language, messages) in map) {
+                    val current = this.map.getOrPut(language) { HashMap() }
+                    current.putAll(messages)
+                }
             }
         }
     }
