e1d568e2638fc3303c1ff4f456187ea3a0447431
MrLarkyy_ <studiotadikl@gmail.com>
2026-01-15 01:14:19 +0100

    Add PaginationSettings to support paginated messages
    Introduces the PaginationSettings class to facilitate handling of paginated messages with customizable headers and footers. Updated AquaticMessageSerializer to integrate with this new functionality.

---
+++ Paper/src/main/kotlin/gg/aquatic/klocale/impl/paper/PaginationSettings.kt
@@ -0,0 +1,10 @@
+package gg.aquatic.klocale.impl.paper
+
+import net.kyori.adventure.text.Component
+
+data class PaginationSettings(
+    val pageSize: Int,
+    val header: Component? = null,
+    val footer: Component? = null
+) {
+}
